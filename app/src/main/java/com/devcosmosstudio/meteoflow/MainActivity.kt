d00a5e0281c3c049ba04c14432e7bd241f9be930
Mohammed Yusuf <mohammedyusufmujawar92@gmail.com>
2025-03-01 17:01:55 -0600

    - Added Internet permission and also added packages for the app tp have a proper structure.

--- app/src/main/java/com/devcosmosstudio/meteoflow/MainActivity.kt
+++ app/src/main/java/com/devcosmosstudio/meteoflow/MainActivity.kt
@@ -4,13 +4,21 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.displayCutoutPadding
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeContent
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.devcosmosstudio.meteoflow.ui.theme.MeteoFlowTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,7 +27,10 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             MeteoFlowTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                Scaffold(
+                    modifier = Modifier.fillMaxSize(),
+                    contentWindowInsets = WindowInsets.safeContent
+                ) { innerPadding ->
                     Greeting(
                         name = "Android",
                         modifier = Modifier.padding(innerPadding)
@@ -32,16 +43,26 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+    Surface(modifier = modifier.padding(4.dp)) {
+        Column {
+            Text(
+                text = "Hello $name!",
+            )
+            Image(
+                painter = painterResource(R.drawable.ic_launcher_background),
+                contentDescription = null
+            )
+            Text(text = "Hello there")
+        }
+    }
 }
 
-@Preview(showBackground = true)
+@Preview(showSystemUi = true)
 @Composable
 fun GreetingPreview() {
     MeteoFlowTheme {
-        Greeting("Android", modifier = Modifier.fillMaxSize())
+        Greeting("Android", modifier = Modifier
+            .fillMaxSize()
+            .displayCutoutPadding())
     }
 }
